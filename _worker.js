d1ef2894e780a2234fac1d395c47ef54e4ba609f
jiexii <913698989@qq.com>
2022-10-15 22:42:22 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="lepoi.com";
+        url.hostname="hrv4.jiexi.ga:1080";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
